69ddc027d739f8b2850df8d60886cf3d15a098a0
Ian Sobo <dev.ian.sobo@gmail.com>
2015-10-19 16:26:04 -0400

    Initial commit with java lib and android lib

---
+++ jokeslib/src/main/java/com/example/JokesContainer.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class JokesContainer {
+
+    public String getJoke() {
+        return "An older man walks into a bar wearing a stovepipe hat, a waistcoat and a phony beard. He sits down at a bar and orders a drink. As the bartender sets it down, he asks, \"Going to a party?\"\n" +
+                "\n" +
+                "\"Yeah, a costume party,\" the man answers, \"I'm supposed to come dressed as my love life.\"\n" +
+                "\n" +
+                "\"But you look like Abe Lincoln,\" protests the bartender.\n" +
+                "\n" +
+                "\"That's right. My last four scores were seven years ago.\"";
+    }
+
+}
