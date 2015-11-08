79c17430c14744723e285b57fd0e2bfd4272aa0b
Ian Sobo <dev.ian.sobo@gmail.com>
2015-11-08 15:57:27 -0500

    Unit tests are verifying that AsyncTask is returning a non-null joke String.

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
@@ -1,6 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
-import android.test.AndroidTestCase;
+import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.MediumTest;
 
 import java.util.concurrent.ExecutionException;
@@ -10,28 +10,33 @@ import java.util.concurrent.TimeoutException;
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends AndroidTestCase {
-    JokeEndpointsAsyncTask asyncTask;
+public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    //JokeEndpointsAsyncTask asyncTask;
     public ApplicationTest() {
-        super();
+        super(MainActivity.class);
     }
 
-    @Override
+/*    @Override
     protected void setUp() throws Exception {
         super.setUp();
         asyncTask = new JokeEndpointsAsyncTask();
 
-    }
+    }*/
     @MediumTest
     public void testAsync() {
         String joke = null;
+        JokeEndpointsAsyncTask asyncTask = new JokeEndpointsAsyncTask();
         try {
-            asyncTask.execute();
+            //MockContext mockContext = new MockContext();
+           // Context context = getInstrumentation().getTargetContext().getApplicationContext();
+            MainActivity mainActivity = getActivity();
+            asyncTask.execute(mainActivity);
             joke =  asyncTask.get(30, TimeUnit.SECONDS);
+            assertNotNull("Joke String was returned as null", joke);
         } catch (InterruptedException | ExecutionException | TimeoutException e) {
             e.printStackTrace();
             fail("Timeout");
         }
-        assertNotNull("Joke String was returned as null", joke);
+
     }
 }
