89454015831c1cacf0e5e2b651cb90aae211e03a
ian <dev.ian.sobo@gmail.com>
2015-11-08 15:10:36 -0500

    Added more jokes

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
@@ -1,12 +1,37 @@
 package com.udacity.gradle.builditbigger;
 
-import android.test.InstrumentationTestCase;
+import android.test.AndroidTestCase;
+import android.test.suitebuilder.annotation.MediumTest;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends InstrumentationTestCase {
+public class ApplicationTest extends AndroidTestCase {
+    JokeEndpointsAsyncTask asyncTask;
     public ApplicationTest() {
         super();
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        asyncTask = new JokeEndpointsAsyncTask();
+
+    }
+    @MediumTest
+    public void testAsync() {
+        String joke = null;
+        try {
+            asyncTask.execute();
+            joke =  asyncTask.get(30, TimeUnit.SECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            e.printStackTrace();
+            fail("Timeout");
+        }
+        assertNotNull("Joke String was returned as null", joke);
     }
 }
