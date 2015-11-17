d8bf1891660e22cb85fbbd8a5a985efda991b7f5
Ian Sobo <dev.ian.sobo@gmail.com>
2015-11-17 17:11:46 -0500

    Button disabling and re-enableing is working correctly

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
@@ -1,10 +1,11 @@
 package com.udacity.gradle.builditbigger;
 
-/*import android.content.Context;
+//import com.udacity.gradle.builditbigger.paid.MainActivity;
+
+import android.content.Context;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.MediumTest;
 import android.util.Log;
-import android.util.Pair;
 import android.widget.Button;
 
 import com.udacity.gradle.builditbigger.free.MainActivity;
@@ -13,18 +14,18 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-//import com.udacity.gradle.builditbigger.paid.MainActivity;
+;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
-
+*/
 public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivity> {
-    //JokeEndpointsAsyncTask asyncTask;
+    JokeEndpointsAsyncTask asyncTask;
     public ApplicationTest() {
         super(MainActivity.class);
     }
 
-/*    @Override
+    @Override
     protected void setUp() throws Exception {
         super.setUp();
         asyncTask = new JokeEndpointsAsyncTask();
@@ -40,7 +41,7 @@ public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivi
             MainActivity mainActivity = getActivity();
             Context context = getInstrumentation().getTargetContext().getApplicationContext();
             testTellJoke = (Button) mainActivity.findViewById(R.id.freeTellJokeButtonId);
-            asyncTask.execute(Pair.create((Context)mainActivity, testTellJoke));
+
             joke =  asyncTask.get(35, TimeUnit.SECONDS);
             assertNotNull("Joke String was returned as null", joke);
             Log.d("ACTIVITY TEST", joke);
@@ -49,4 +50,4 @@ public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivi
             fail("Timeout");
         }
     }
-}*/
+}
